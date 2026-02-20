0c6922f1e332585594e58fee6ccf2f551802f1a3
imaginov <134341734+pascalino007@users.noreply.github.com>
2026-02-20 14:21:13 +0000

    Delete akodessewa-api directory

--- akodessewa-api/src/garage/garage.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GarageService } from './garage.service';
-import { GarageController } from './garage.controller';
-
-@Module({
-  controllers: [GarageController],
-  providers: [GarageService],
-})
-export class GarageModule {}
