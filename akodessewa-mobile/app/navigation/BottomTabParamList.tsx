1889c38443bb5d4a9390645592aff22a0e793ca3
imaginov <134341734+pascalino007@users.noreply.github.com>
2026-02-27 23:25:37 +0000

    Delete akodessewa-mobile directory

--- akodessewa-mobile/app/navigation/BottomTabParamList.tsx
+++
@@ -1,7 +0,0 @@
-export type BottomTabParamList = {
-    Home: undefined;
-    Wishlist: undefined;
-    MyCart: undefined;
-    Category: undefined;
-    Profile: undefined;
-};
--- akodessewa-mobile/app/navigation/DrawerParamList.tsx
+++
@@ -1,3 +0,0 @@
-export type DrawerParamList = {
-    BottomTab: undefined;
-};
